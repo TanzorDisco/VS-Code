ead876fdafc6625f4371b34870a17977e75c0881
TanzorDisco <sungurov.islam@gmail.com>
2021-08-22 17:03:33 +0300

    Create Console.ProgressBar.cs

---
+++ Console.ProgressBar.cs
@@ -0,0 +1,39 @@
+class Program
+	{
+		const char _block = '■';
+		const string _back = "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b";
+
+		static async Task Main(string[] args)
+		{
+			Console.Write("Something is out: ");
+
+			WriteProgressBar(0);
+
+			for (var i = 0; i <= 100; ++i)
+			{
+				WriteProgressBar(i, true);
+				Thread.Sleep(50);
+			}
+
+			Console.WriteLine();
+
+			await Task.CompletedTask;
+		}
+
+
+		public static void WriteProgressBar(int percent, bool update = false)
+		{
+			if (update)
+				Console.Write(_back);
+			Console.Write("[");
+			var p = (int)((percent / 10f) + .5f);
+			for (var i = 0; i < 10; ++i)
+			{
+				if (i >= p)
+					Console.Write(' ');
+				else
+					Console.Write(_block);
+			}
+			Console.Write("] {0,3:##0}%", percent);
+		}
+	}
